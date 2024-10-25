b1474d940153bef0594edd9e355dd7fa4106e241
guannant <guannant@andrew.cmu.edu>
2024-10-25 10:44:45 -0500

    setup.py updated

--- setup.py
+++ setup.py
@@ -67,5 +67,9 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
